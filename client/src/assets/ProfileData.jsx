9212288a389340fab621e2f761fbddb42281ce44
Priyanshu1502 <priyanshubhatt1502@gmail.com>
2024-04-18 20:27:13 +0530

    starting to work on profile page

---
+++ client/src/assets/ProfileData.jsx
@@ -0,0 +1,10 @@
+const ProfileData = [
+  {
+    notification1: "event #1 for the alumni's",
+    notification2: "event #2 for the alumni's",
+    profileImg: "https://source.unsplash.com/random/?kitchen",
+    backgroundImg: "https://source.unsplash.com/random/?food",
+    comment1: "loved this post",
+    comment2: "loved this post",
+  },
+];
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import TopProfile from "../components/TopProfile";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="bg-[#e3e3e3] w-full">
+      <div className="bg-white">
+        <TopProfile />
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
